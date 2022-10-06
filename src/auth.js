934cf81811db14e933dc6255613812d5e20d07e8
Zac Roberts <1752414+zac-roberts@users.noreply.github.com>
2022-10-06 10:36:58 -0500

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -2,15 +2,36 @@ var cc = DataStudioApp.createCommunityConnector();
 
 // https://developers.google.com/datastudio/connector/reference#getauthtype
 /**
- * Returns the authentication method required by the connector to authorize the
- * third-party service.
- *
- * @returns {Object} `AuthType` used by the connector.
+ * Returns the Auth Type of this connector.
+ * @return {object} The Auth type.
  */
 function getAuthType() {
-  var AuthTypes = cc.AuthType;
-  return cc
-    .newAuthTypeResponse()
-    .setAuthType(AuthTypes.NONE)
-    .build();
+  var cc = DataStudioApp.createCommunityConnector();
+  return cc.newAuthTypeResponse()
+      .setAuthType(cc.AuthType.USER_PASS)
+      .setHelpUrl('TBD')
+      .build();
+}
+
+/**
+ * Resets the auth service.
+ */
+function resetAuth() {
+  var userProperties = PropertiesService.getUserProperties();
+  userProperties.deleteProperty('dscc.username');
+  userProperties.deleteProperty('dscc.password');
+}
+
+/**
+ * Returns true if the auth service has access.
+ * @return {boolean} True if the auth service has access.
+ */
+function isAuthValid() {
+  var userProperties = PropertiesService.getUserProperties();
+  var userName = userProperties.getProperty('dscc.username');
+  var password = userProperties.getProperty('dscc.password');
+  // This assumes you have a validateCredentials function that
+  // can validate if the userName and password are correct.
+  // TODO return validateCredentials(userName, password);
+  return true //TODO 
 }
